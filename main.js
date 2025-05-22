6b2b8e3a00f266148e0d149bc935978e851b63c0
Musho <musho200214@gmail.com>
2025-05-22 21:38:22 +0400

    Left menu ended

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("asd")
+document.querySelectorAll('.leftContentMenus a').forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      document.querySelectorAll('.leftContentMenus a').forEach(function(a) {
+        a.classList.remove('activeBtn');
+      });
+  
+      this.classList.add('activeBtn');
+    });
+  });

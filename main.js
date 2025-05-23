7a80ede89f1ee1e1bf82913a475077c506640157
Musho <musho200214@gmail.com>
2025-05-23 21:05:40 +0400

    Done Task

--- main.js
+++ main.js
@@ -9,3 +9,51 @@ document.querySelectorAll('.leftContentMenus a').forEach(function(link) {
       this.classList.add('activeBtn');
     });
   });
+
+const inputId = document.getElementsByClassName("searchId")[0];
+const inputName = document.getElementsByClassName("searchName")[0];
+const select = document.getElementById("selectFilter");
+const rows = document.querySelectorAll(".productTable tr:nth-child(n+3)");
+
+inputId.addEventListener("input", function () {
+    const value = this.value.toUpperCase();
+    rows.forEach(row => {
+        const id = row.children[0].textContent.toLowerCase(); 
+        if (!value || id.includes(value)) {
+        row.style.display = "";
+        } else {
+        row.style.display = "none";
+        }
+    });
+});
+
+
+inputName.addEventListener("input", function () {
+    const value = this.value.toLowerCase();
+        rows.forEach(row => {
+            const name = row.children[1].textContent.toLowerCase(); 
+            if (!value || name.includes(value)) {
+            row.style.display = "";
+            } else {
+            row.style.display = "none";
+            }
+        });
+    });
+
+    
+select.addEventListener("change", function () {
+  const value = this.value.toLowerCase();
+  if (value) {
+    this.style.color = "#767676"; 
+  } else {
+    this.style.color = ""; 
+  }
+  rows.forEach(row => {
+    const cuisine = row.children[2].textContent.toLowerCase(); 
+    if (!value || cuisine.includes(value)) {
+      row.style.display = "";
+    } else {
+      row.style.display = "none";
+    }
+  });
+});
